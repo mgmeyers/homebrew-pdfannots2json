b54bd5f1252d92f03c024ad75e125979462f72e8
mgmeyers <matthew.gregory.meyers@gmail.com>
2022-04-26 12:29:43 -0700

    Update formula

--- Formula/recoll.rb
+++ Formula/recoll.rb
@@ -1,19 +1,9 @@
-require 'formula'
-# Notes:
-# - This formula is missing python-libxml2 and python-libxslt deps
-#   which recoll needs for indexing many formats (e.g. libreoffice,
-#   openxml). Homebrew does not include these packages.
-#   So the user needs to install them with pip because I don't understand how
-#   the "Resource" homebrew thing works.
-# Still a bit of work then, but I did not investigate, because the macports
-# version was an easier target.
-
-#  copy from https://framagit.org/medoc92/recoll/-/blob/master/packaging/homebrew/recoll.rb
 class Recoll < Formula
   desc "Desktop search tool"
-  homepage 'http://www.recoll.org'
-  url 'https://www.lesbonscomptes.com/recoll/recoll-1.30.1.tar.gz'
-  sha256 "835f06fea60d95cb6238a4f38937b506b33867b35c7808e9e4f335f4cd2c5d63"
+  homepage "http://www.recoll.org"
+  url "https://www.lesbonscomptes.com/recoll/recoll-1.32.0.tar.gz"
+  sha256 "e24b7a839315653c982d21fbab6cfbda7cc0b14e9f6ad0b4bd379b10a85203a1"
+  license "GPL"
 
   depends_on "xapian"
   depends_on "qt@5"
@@ -22,15 +12,16 @@ class Recoll < Formula
   depends_on "unrtf"
   depends_on "aspell"
   depends_on "exiftool"
+  depends_on "libxml2"
+  depends_on "libxslt"
+  depends_on "unzip"
 
   def install
-    # homebrew has webengine, not webkit and we're not ready for this yet
     system "./configure", "--disable-webkit",
                           "--disable-python-chm",
                           "--disable-qtgui",
                           "--prefix=#{prefix}"
     system "make", "install"
-#    bin.install "#{buildpath}/qtgui/recoll.app/Contents/MacOS/recoll"
   end
 
   test do
