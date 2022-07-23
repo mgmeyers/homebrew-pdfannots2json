744b10690c33410315fc077575f543e2acbd55cf
mgmeyers <matthew.gregory.meyers@gmail.com>
2022-07-23 10:25:57 -0700

    Bump

--- Formula/pdfannots2json.rb
+++ Formula/pdfannots2json.rb
@@ -3,8 +3,8 @@ class Pdfannots2json < Formula
   homepage "https://github.com/mgmeyers/pdfannots2json"
   license "AGPL-3.0"
 
-  url "https://github.com/mgmeyers/pdfannots2json/archive/refs/tags/1.0.8.tar.gz"
-  sha256 "2c7c67c3c33db9feb32c610d7a97a4c127e2c4bf24dece8a888fc93b8de6c178"
+  url "https://github.com/mgmeyers/pdfannots2json/archive/refs/tags/1.0.9.tar.gz"
+  sha256 "4e1e6374e59c2e3000d83398364a577162d6481e33ba719dce8a0ac582f02a4d"
   head "https://github.com/mgmeyers/pdfannots2json"
 
   depends_on "tesseract"
@@ -21,6 +21,6 @@ class Pdfannots2json < Formula
   end
 
   test do
-    assert_match "v1.0.8", shell_output("#{bin}/pdfannots2json -v 2>&1", 2)
+    assert_match "v1.0.9", shell_output("#{bin}/pdfannots2json -v 2>&1", 2)
   end
 end
